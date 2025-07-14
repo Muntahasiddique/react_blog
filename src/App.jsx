6de81b6527c6b0b3cc68927603c2929322e6681a
muntaha <muntahamirza890@gmail.com>
2025-07-14 15:26:42 +0500

    Added Counter Button

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import user from './user'
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>Code step by step</h1>
-    </>
-  )
+  const [counter,setCounter] = useState(0);
+return <>
+<h1>Counter Val:{counter}</h1>
+<h1>react Componenet</h1>
+<user/>
+<button onClick={()=>setCounter(counter+1)} >increase Counter Value</button>
+</>
+  
 }
 
 export default App
---
+++ src/user.jsx
@@ -0,0 +1,5 @@
+function user(){
+    return <h1>User Componenet</h1>
+}
+
+export default user;
