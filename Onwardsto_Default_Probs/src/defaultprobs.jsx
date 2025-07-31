6e46a663d85d6b1a9e429eac92a265e1d6f0e744
muntaha <muntahamirza890@gmail.com>
2025-07-31 22:38:16 +0500

    Added Default probs with jsx and styling as well

---
+++ Onwardsto_Default_Probs/src/defaultprobs.jsx
@@ -0,0 +1,17 @@
+// export function Defaultprop({name ="uSER"}){
+// return(
+//     <>
+//     <h1>hi, {name}</h1>
+//     </>
+// )
+// }
+
+
+// for passing jsx
+export function Defaultprop({children,color="purple"}){
+return(
+    <div style={{color:color}} >
+    {children}
+    </div>
+)
+}
