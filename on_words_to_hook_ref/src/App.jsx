d37fff0e1b571678ea742a446594bf8a24094373
muntaha <muntahamirza890@gmail.com>
2025-08-03 02:31:45 +0500

    Added form submissin by unctrolled components like DOM manipulation and  hookref

--- on_words_to_hook_ref/src/App.jsx
+++ on_words_to_hook_ref/src/App.jsx
@@ -6,13 +6,45 @@ import './App.css'
 function App() {
 const inputuseRef= useRef(null)
 
-function chnagecolor (){
-inputuseRef.current.focus();
-}
+// function chnagecolor (){
+// inputuseRef.current.focus();
+// }
+
+   function submitwithotref(event){
+       event.preventDefault()
+       const user = document.querySelector("#name")
+console.log("user")
+   }
+ function submitref(event){
+    event.preventDefault()
+    const refname =inputuseRef.current.value
+console.log(refname)
+ }
+
   return (
     <>
-    <button onClick={chnagecolor} >foucous</button>
-    <input ref={inputuseRef}  type="text"  placeholder='Enter something' />
+    {/* <button onClick={chnagecolor} >foucous</button>
+    <input ref={inputuseRef}  type="text"  placeholder='Enter something' /> */}
+    {/* .
+    .
+    .
+    .
+     .
+    .
+    .
+    . */}
+    {/* unControlled Components */}
+    <form action="" method='post' onSubmit={submitwithotref} >
+      <input type="text" id='name' placeholder='Enter name'/>
+        <input type="text" id='pass' placeholder='Enter password'/>
+        <button>Submit</button>
+    </form>
+    <br />
+    <form action="" method='post' onSubmit={submitref} >
+      <input type="text" ref={ inputuseRef} id='name' placeholder='Enter name'/>
+        <input type="text" id='pass' placeholder='Enter password'/>
+        <button>Submit</button>
+    </form>
     </>
   )
 }
