676b471a58a233d132de0feb5026de99fd13cced
muntaha <muntahamirza890@gmail.com>
2025-08-03 02:08:08 +0500

    Added  hookref

--- on_words_to_hook_ref/src/App.jsx
+++ on_words_to_hook_ref/src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
+import { useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const inputuseRef= useRef(null)
 
+function chnagecolor (){
+inputuseRef.current.focus();
+}
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <button onClick={chnagecolor} >foucous</button>
+    <input ref={inputuseRef}  type="text"  placeholder='Enter something' />
     </>
   )
 }
