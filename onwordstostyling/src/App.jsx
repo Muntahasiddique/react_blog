da21fd5dbfa9399123f9cbb4aa56db09ccf51db7
muntaha <muntahamirza890@gmail.com>
2025-08-03 00:14:11 +0500

    Added raect with External Css

--- onwordstostyling/src/App.jsx
+++ onwordstostyling/src/App.jsx
@@ -2,92 +2,97 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './css/styles.css'
 
 function App() {
- const originalStyle ={
-    border: "1px solid #cccccc3b",
-    width: "200px",
-    boxShadow: "1px 2px 3px 0px #cccccc57",
-    margin: "10px"
-  }
-    const  [grid, setgridstyle]  = useState(true)
-  const [cardStyle, setcardStyle] = useState({
-    originalStyle
-  }); // Added missing state
+//  const originalStyle ={
+//     border: "1px solid #cccccc3b",
+//     width: "200px",
+//     boxShadow: "1px 2px 3px 0px #cccccc57",
+//     margin: "10px"
+//   }
+//     const  [grid, setgridstyle]  = useState(true)
+//   const [cardStyle, setcardStyle] = useState({
+//     originalStyle
+//   }); // Added missing state
 
-function changingcolor(){
-setcardStyle({...cardStyle,backgroundColor:'blue'})
-}
-function originalcolor(){
-setcardStyle({originalStyle})
-}
+// function changingcolor(){
+// setcardStyle({...cardStyle,backgroundColor:'blue'})
+// }
+// function originalcolor(){
+// setcardStyle({originalStyle})
+// }
 
   return (
-    <div>
-      <button onClick={changingcolor}  >Grey</button>
-         <button onClick={originalcolor}  >Original</button>
-          <button onClick={()=>setgridstyle(!grid)}  >Toggle</button>
+    <>
+    {/* // <div>
+    //   <button onClick={changingcolor}  >Grey</button>
+    //      <button onClick={originalcolor}  >Original</button>
+    //       <button onClick={()=>setgridstyle(!grid)}  >Toggle</button>
 
-    <h1 style={{ color: 'red' }}>Dynamic and Conditional Inline Style</h1>
-    <div style={{ display:grid? 'flex':'block', flexWrap: 'wrap' }}>
-      <div style={cardStyle}>
-        <img 
-          style={{ width: '200px' }} 
-          src="https://www.w3schools.com/howto/img_avatar.png" 
-          alt="Profile"
-        />
-        <div style={{ padding: '5px' }}>
-          <h4>Anil Sidhu</h4>
-          <p>Software developer</p>
-        </div>
-      </div>
-        <div style={cardStyle}>
-        <img 
-          style={{ width: '200px' }} 
-          src="https://www.w3schools.com/howto/img_avatar.png" 
-          alt="Profile"
-        />
-        <div style={{ padding: '5px' }}>
-          <h4>Anil Sidhu</h4>
-          <p>Software developer</p>
-        </div>
-      </div>
-        <div style={cardStyle}>
-        <img 
-          style={{ width: '200px' }} 
-          src="https://www.w3schools.com/howto/img_avatar.png" 
-          alt="Profile"
-        />
-        <div style={{ padding: '5px' }}>
-          <h4>Anil Sidhu</h4>
-          <p>Software developer</p>
-        </div>
-      </div>
-        <div style={cardStyle}>
-        <img 
-          style={{ width: '200px' }} 
-          src="https://www.w3schools.com/howto/img_avatar.png" 
-          alt="Profile"
-        />
-        <div style={{ padding: '5px' }}>
-          <h4>Anil Sidhu</h4>
-          <p>Software developer</p>
-        </div>
-      </div>
-        <div style={cardStyle}>
-        <img 
-          style={{ width: '200px' }} 
-          src="https://www.w3schools.com/howto/img_avatar.png" 
-          alt="Profile"
-        />
-        <div style={{ padding: '5px' }}>
-          <h4>Anil Sidhu</h4>
-          <p>Software developer</p>
-        </div>
-      </div>
-    </div>
+    // <h1 style={{ color: 'red' }}>Dynamic and Conditional Inline Style</h1>
+    // <div style={{ display:grid? 'flex':'block', flexWrap: 'wrap' }}>
+    //   <div style={cardStyle}>
+    //     <img 
+    //       style={{ width: '200px' }} 
+    //       src="https://www.w3schools.com/howto/img_avatar.png" 
+    //       alt="Profile"
+    //     />
+    //     <div style={{ padding: '5px' }}>
+    //       <h4>Anil Sidhu</h4>
+    //       <p>Software developer</p>
+    //     </div>
+    //   </div>
+    //     <div style={cardStyle}>
+    //     <img 
+    //       style={{ width: '200px' }} 
+    //       src="https://www.w3schools.com/howto/img_avatar.png" 
+    //       alt="Profile"
+    //     />
+    //     <div style={{ padding: '5px' }}>
+    //       <h4>Anil Sidhu</h4>
+    //       <p>Software developer</p>
+    //     </div>
+    //   </div>
+    //     <div style={cardStyle}>
+    //     <img 
+    //       style={{ width: '200px' }} 
+    //       src="https://www.w3schools.com/howto/img_avatar.png" 
+    //       alt="Profile"
+    //     />
+    //     <div style={{ padding: '5px' }}>
+    //       <h4>Anil Sidhu</h4>
+    //       <p>Software developer</p>
+    //     </div>
+    //   </div>
+    //     <div style={cardStyle}>
+    //     <img 
+    //       style={{ width: '200px' }} 
+    //       src="https://www.w3schools.com/howto/img_avatar.png" 
+    //       alt="Profile"
+    //     />
+    //     <div style={{ padding: '5px' }}>
+    //       <h4>Anil Sidhu</h4>
+    //       <p>Software developer</p>
+    //     </div>
+    //   </div>
+    //     <div style={cardStyle}>
+    //     <img 
+    //       style={{ width: '200px' }} 
+    //       src="https://www.w3schools.com/howto/img_avatar.png" 
+    //       alt="Profile"
+    //     />
+    //     <div style={{ padding: '5px' }}>
+    //       <h4>Anil Sidhu</h4>
+    //       <p>Software developer</p>
+    //     </div>
+    //   </div>
+    // </div>
     
-    </div>
+    // </div> */}
+<h1 className='profile' >Profile</h1>
+
+    </>
   )
 }
 
