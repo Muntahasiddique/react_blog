c3379c4fe39c39bd56f25b5d0a63e322fe55e4ef
muntaha <muntahamirza890@gmail.com>
2025-08-04 03:27:04 +0500

    Added useformstatus for delayed submitting forms

--- pass_function_in_Components_asProps/src/App.jsx
+++ pass_function_in_Components_asProps/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 // import { PassFunctions } from './passfunctions'
 import { Userefun } from './useRef'
+import { useFormStatus } from 'react-dom'
 function App() {
   // PassFunasprop
 //  function PassFunasprop(name){
@@ -18,9 +19,34 @@ function App() {
 // .
 // .
 // Fowrad Ref
-const inputref  = useRef(null)
-function Updateusingref(){
-  inputref.current.value ="10000"
+// const inputref  = useRef(null)
+// function Updateusingref(){
+//   inputref.current.value ="10000"
+// }
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// { useFormStatus 
+function FormComp (){
+     const {pending} =useFormStatus();
+  return(
+    <>
+  <input type="text " placeholder='enter' />
+  <button disabled={pending}  >{pending ?'SUBMITTING':'SUBMIT' }  </button>
+ 
+    </>
+  )
+  
+}
+
+
+async  function handlesubmit(){
+  await new Promise(res =>setTimeout(res,5000))
 }
   return (
     <>
@@ -35,8 +61,22 @@ function Updateusingref(){
 // .
 // . */}
   {/* // forwod ref */}
-  <h1>Fowraed ref</h1>
-<Userefun  inputref={ inputref} Updateusingref={Updateusingref} />
+  {/* <h1>Fowraed ref</h1>
+<Userefun  inputref={ inputref} Updateusingref={Updateusingref} /> */}
+
+{/* // .
+// .
+// .
+// .
+// .
+// .
+// .
+// . */}
+{/* // Use FormStatus hook*/}
+    <form action={handlesubmit}>
+   
+<FormComp />
+</form>
 
     </>
   )
