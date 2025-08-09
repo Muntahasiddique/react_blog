d3355df342f8ea823a556e0d0a84c87f6c96dfc8
muntaha <muntahamirza890@gmail.com>
2025-08-10 01:17:46 +0500

    Added the update functionality and CRUD Completed

--- APIOnword/src/edituser.jsx
+++ APIOnword/src/edituser.jsx
@@ -1,17 +1,19 @@
 import { useEffect } from "react"
-import { useParams } from "react-router"
+import { useNavigate, useParams } from "react-router"
 import { useState } from "react"
 export function EditUser() {
   const [name , setname] = useState('');
   const [email , setemail] = useState('');
   const [age, setage] = useState('');
+  const nevigate = useNavigate()
 const {id} = useParams()
+  const url = "http://localhost:3000/users/"+id
 useEffect(()=>{
 getUserData()
 },[])
 async function getUserData()
 {
-    const url = "http://localhost:3000/users/"+id
+  
     let response = await fetch(url)
     response = await response.json()
 
@@ -19,18 +21,35 @@ setname(response.name)
 setage(response.age)
 setemail(response.email)
 
-}  return (
+}  
+async function UpdateEdit(){
+let response = await fetch(url ,{
+  method:"Put",
+  body:JSON.stringify({
+    name,
+    age,
+    email
+  })} );
+response = await response.json();
+if(response){
+  alert("edited")
+nevigate('/')
+}
+
+}
+
+return (
     <>
     <h1>Edit</h1>
-    <input value={name} type="text " placeholder="Add name " />
+    <input value={name}  onChange={(event)=>setname(event.target.value)} type="text " placeholder="Add name " />
   <br />
   <br />
-   <input value={email} type="text " placeholder="Add email " />
+   <input  onChange={(event)=>setemail(event.target.value)}  value={email} type="text " placeholder="Add email " />
   <br />
   <br />
-   <input value={age} type="text " placeholder="Add age" />
+   <input onChange={(event)=>setage(event.target.value)}  value={age} type="text " placeholder="Add age" />
   <br />
   <br />
-  <button > Edit</button>
+  <button onClick={UpdateEdit} > Edit</button>
     
     </>)}
