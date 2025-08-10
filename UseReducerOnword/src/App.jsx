5132a3c1ee3f74e0136dc7ea92e4f6469dc9eff0
muntaha <muntahamirza890@gmail.com>
2025-08-11 02:06:52 +0500

    Added  use reducer

--- UseReducerOnword/src/App.jsx
+++ UseReducerOnword/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
+import { useReducer, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+const UseData = {
+  name :'',
+  password:'',
+  Email:'',
+  City:''
 
+}
+function reducerfunc(data,action){
+return{...data,[action.type]:action.val}
+}
 function App() {
-  const [count, setCount] = useState(0)
+ const [state , dispatch] = useReducer(reducerfunc,UseData);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+   <form action="">
+    <input onChange={(event)=>dispatch({val:event.target.value,type:'name'})}  type="text" placeholder='name' />
+     <input  onChange={(event)=>dispatch({val:event.target.value,type:"password"})}  type="text" placeholder='password' />
+      <input onChange={(event)=>dispatch({val:event.target.value,type:"email"})}  type="text" placeholder='Email' />
+       <input  onChange={(event)=>dispatch({val:event.target.value,type:"city"})}  type="text" placeholder='city' />
+       <button >add details</button>
+         <li>name:{state.name}</li>
+          <li>pass:{state.password}</li>
+           <li>city:{state.city}</li>
+            <li>email:{state.email}</li>
+   </form>
+
     </>
   )
 }
